af2aa1633326311c68ef88ce743d05c9da2ba92c
user <shafiqrasool104@gmail.com>
2023-08-10 15:21:49 +0500

    Initial commit

---
+++ app/src/main/java/com/shafirqrasool/chatapplication/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.shafirqrasool.chatapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    val binding:
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
